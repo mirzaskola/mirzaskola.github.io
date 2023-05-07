dae3ce9255775788d561d4092eb2ab1a2ee3cd89
mirzaskola <mirza.kurtovic@stu.ibu.edu.ba>
2023-05-07 21:44:01 +0200

    Port number updated

--- rest-api/Config.class.php
+++ rest-api/Config.class.php
@@ -15,7 +15,7 @@ class Config {
     return Config::get_env("DB_SCHEME", "sql7616663");
   }
   public static function DB_PORT(){
-    return Config::get_env("DB_PORT", "3310");
+    return Config::get_env("DB_PORT", "3306");
   }
   public static function JWT_SECRET(){
     return Config::get_env("JWT_SECRET", "ezcb9s8UcF");
